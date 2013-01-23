b1b1c5233d7beec1c5e55237c05636d92f383d56
John.Roark <john.roark@gmail.com>
2013-01-22 16:58:18 -0800

    * PS2 mouse overwhelms the Teensy

--- ps2.c
+++ ps2.c
@@ -75,9 +75,29 @@
 #define PS2KBDATA_CONFIG_OUT (DDRD |= (1<<PS2KBDATA))
 #define PS2KBDATA_CONFIG_IN  (DDRD &= ~(1<<PS2KBDATA))
 
-/* pins for PS2 mouse CLK/DATA */
-/* TODO */
+/* this will overwhelm the Teensy */
+#define USE_MOUSE 0
 
+#if USE_MOUSE
+/* pins for PS2 mouse CLK/DATA */
+#define PS2MSCLK   1 /*PD1 == 6*/
+#define PS2MSDATA  6 /*PC6 == 9*/
+
+#define PS2MSCLK_HI          (PORTD |= (1<<PS2MSCLK))
+#define PS2MSCLK_LO          (PORTD &= ~(1<<PS2MSCLK))
+#define PS2MSDATA_HI         (PORTC |= (1<<PS2MSDATA))
+#define PS2MSDATA_LO         (PORTC &= ~(1<<PS2MSDATA))
+
+#define PS2MSDATA_ISHI       ((PINC & (1<<PS2MSDATA)) ? 1 : 0)
+
+#define PS2MSCLK_CONFIG_OUT  (DDRD |= (1<<PS2MSCLK))
+#define PS2MSCLK_CONFIG_IN   (DDRD &= ~(1<<PS2MSCLK))
+
+#define PS2MSCLK_ISHI        ((PIND & (1<<PS2MSCLK)) ? 1 : 0)
+
+#define PS2MSDATA_CONFIG_OUT (DDRC |= (1<<PS2MSDATA))
+#define PS2MSDATA_CONFIG_IN  (DDRC &= ~(1<<PS2MSDATA))
+#endif
 /* pins for NeXT IN/OUT from soundbox */
 #define FROM_NEXT  0 /*PB0 = 0 */
 #define TO_NEXT    1 /*PB1 = 1 */
@@ -98,11 +118,21 @@
 
 #define TIMING 50
 
-#define BUFFER_SIZE 45
-static volatile uint8_t buffer[BUFFER_SIZE];
-static volatile uint8_t head, tail;
+#define BUFFER_SIZE (5*45)
+static volatile uint8_t kb_buffer[BUFFER_SIZE];
+static volatile uint8_t kb_head, kb_tail;
+
+#if USE_MOUSE
+static volatile uint8_t ms_buffer[BUFFER_SIZE];
+static volatile uint8_t ms_head, ms_tail;
+#endif
 
 volatile unsigned long timer0_millis_count = 0; 
+
+#if USE_MOUSE
+static inline uint8_t read_mouse_bit ();
+static uint8_t read_mouse_byte ();
+#endif
 
 /*----------------------------------------------------------------------------*/
 
@@ -141,6 +171,7 @@ ISR (INT0_vect)
     uint32_t        now_ms;
     uint8_t         n, val;
 
+    LED_ON;
     val = PS2KBDATA_ISHI;
     now_ms = millis();
 
@@ -157,17 +188,64 @@ ISR (INT0_vect)
     bitcount++;
     if (bitcount == 11) 
     {
-        uint8_t i = head + 1;
+        uint8_t i = kb_head + 1;
         if (i >= BUFFER_SIZE) i = 0;
-        if (i != tail)
+        if (i != kb_tail)
         {
-            buffer[i] = incoming;
-            head = i;
+            kb_buffer[i] = incoming;
+            kb_head = i;
         }
         bitcount = 0;
         incoming = 0;
     }
+
+    LED_OFF;
 }
+
+/*----------------------------------------------------------------------------*/
+
+// The ISR for the external PS2 Mouse interrupt
+#if USE_MOUSE
+ISR (INT1_vect)
+{
+    static uint8_t  bitcount = 0;
+    static uint8_t  incoming = 0;
+    static uint32_t prev_ms  = 0;
+    uint32_t        now_ms;
+    uint8_t         n, val;
+
+    LED_ON;
+
+    val = PS2MSDATA_ISHI;
+    now_ms = millis();
+
+    if (now_ms - prev_ms > 250)
+    {
+        bitcount = 0;
+        incoming = 0;
+    }
+    prev_ms = now_ms;
+    n = bitcount - 1;
+    if (n <= 7)
+        incoming |= (val << n);
+
+    bitcount++;
+    if (bitcount == 11) 
+    {
+        uint8_t i = ms_head + 1;
+        if (i >= BUFFER_SIZE) i = 0;
+        if (i != ms_tail)
+        {
+            ms_buffer[i] = incoming;
+            ms_head = i;
+        }
+        bitcount = 0;
+        incoming = 0;
+    }
+
+    LED_OFF;
+}
+#endif
 
 /*----------------------------------------------------------------------------*/
 
@@ -176,17 +254,38 @@ get_scan_code (void)
 {
     uint8_t c = 0, i;
 
-    i = tail;
-    if (i == head)
+    i = kb_tail;
+    if (i == kb_head)
         return 0;
     i++;
     if (i >= BUFFER_SIZE) 
         i = 0;
-    c = buffer[i];
-    tail = i;
+    c = kb_buffer[i];
+    kb_tail = i;
 
     return c;
 }
+
+/*----------------------------------------------------------------------------*/
+
+#if USE_MOUSE
+    static inline uint8_t
+get_mouse_code (void)
+{
+    uint8_t c = 0, i;
+
+    i = ms_tail;
+    if (i == ms_head)
+        return 0;
+    i++;
+    if (i >= BUFFER_SIZE) 
+        i = 0;
+    c = ms_buffer[i];
+    ms_tail = i;
+
+    return c;
+}
+#endif
 
 /*----------------------------------------------------------------------------*/
 
@@ -321,6 +420,121 @@ get_modifier (uint8_t code, uint8_t right)
 }
 
 /*----------------------------------------------------------------------------*/
+#if USE_MOUSE
+#define PULL_CLK_HI    { PS2MSCLK_CONFIG_IN; PS2MSCLK_HI; }
+#define PULL_DATA_HI   { PS2MSDATA_CONFIG_IN; PS2MSDATA_HI; }
+
+#define PULL_CLK_LO    { PS2MSCLK_CONFIG_OUT; PS2MSCLK_LO; }
+#define PULL_DATA_LO   { PS2MSDATA_CONFIG_OUT; PS2MSDATA_LO; }
+
+static void write_mouse (uint8_t data)
+{
+    uint8_t i      = 0;
+    uint8_t parity = 1;
+
+    PULL_DATA_HI;
+    PULL_CLK_HI;
+    _delay_us (300);
+    PULL_CLK_LO;
+    _delay_us (300);
+    PULL_DATA_LO;
+    _delay_us (10);
+    PULL_CLK_HI;  // Start Bit
+    _delay_us (10);
+    while (PS2MSCLK_ISHI);// wait for mouse to take control of clock)
+    // clock is low, and we are clear to send data 
+    for (i = 0; i < 8; i++)
+    {
+        if (data & 0x01) {
+            PULL_DATA_HI;
+        } else {
+            PULL_DATA_LO;
+        }
+        // wait for clock cycle 
+        while (!PS2MSCLK_ISHI);
+        while (PS2MSCLK_ISHI);
+        parity = parity ^ (data & 0x01);
+        data = data >> 1;
+    }  
+    // parity 
+    if (parity) {
+        PULL_DATA_HI;
+    } else {
+        PULL_DATA_LO;
+    }
+    while (!PS2MSCLK_ISHI);
+    while (PS2MSCLK_ISHI);
+    PULL_DATA_HI;
+    _delay_us (50);
+
+    while (PS2MSCLK_ISHI);
+    while ((!PS2MSCLK_ISHI) || (!PS2MSDATA_ISHI)); // wait for mouse to switch modes
+    PULL_CLK_LO; // put a hold on the incoming data.
+}
+
+/*----------------------------------------------------------------------------*/
+
+static inline uint8_t read_mouse_bit ()
+{
+    uint8_t bit;
+
+    while (PS2MSCLK_ISHI);
+    bit = PS2MSDATA_ISHI;
+    while (!PS2MSCLK_ISHI);
+
+    return bit;
+}
+
+/*----------------------------------------------------------------------------*/
+
+static uint8_t read_mouse_byte ()
+{
+    uint8_t data = 0;
+    uint8_t i    = 0;
+
+    PULL_CLK_HI;
+    PULL_DATA_HI;
+    _delay_us (50);
+
+    while (PS2MSCLK_ISHI);
+
+    _delay_us (5);
+    while (!PS2MSCLK_ISHI);
+    for (i = 0; i < 8; i++)
+        data |= (read_mouse_bit () << i);
+
+    read_mouse_bit ();
+    read_mouse_bit ();
+
+    return data;
+}
+
+/*----------------------------------------------------------------------------*/
+
+static void init_mouse ()
+{
+    _delay_ms (20);
+    write_mouse (0xFF);
+    read_mouse_byte ();
+    _delay_ms (20);
+    read_mouse_byte ();
+    read_mouse_byte ();
+    _delay_ms (20);
+
+    /* turn the sample rate way down */
+    write_mouse (0xF3);
+    read_mouse_byte ();
+    write_mouse (0x0A);
+    read_mouse_byte ();
+
+    write_mouse (0xF4);
+    read_mouse_byte ();
+
+    _delay_us (100);
+}
+#endif
+
+/*----------------------------------------------------------------------------*/
 
 int main (void)
 {
@@ -344,13 +558,27 @@ int main (void)
     PS2KBCLK_HI;
     PS2KBDATA_CONFIG_IN;
     PS2KBDATA_HI;
-  
-    head = 0;
-    tail = 0;
 
-    // falling edge for PS2 int
+#if USE_MOUSE
+    PULL_DATA_HI;
+    PULL_CLK_HI;
+
+    init_mouse ();
+
+    ms_head = 0;
+    ms_tail = 0;
+#endif
+
+    kb_head = 0;
+    kb_tail = 0;
+
+    // falling edge for PS2 KB/Mouse int
     EICRA = (EICRA & 0xFC) | (0x02&0x03);
     EIMSK |= (1 << PS2KBCLK);
+#if USE_MOUSE
+    EIMSK |= (1 << PS2MSCLK);
+#endif
+
     sei ();
 
     while (1)
@@ -406,7 +634,7 @@ int main (void)
                 {
                     modifiers |= mod;
                     resp = 0x00300600;
-                    goto send_response;
+                    goto send_kb_response;
                 }
                 resp = 0x00280400;
             }
@@ -417,25 +645,25 @@ int main (void)
 
                 if (0 == ps2_scancode)
                 {
-                    tail--;
+                    kb_tail--;
                     resp = 0x00300600;
-                    goto send_response;
+                    goto send_kb_response;
                 }
                 else if (ps2_scancode == 0xF0)
                 {
                     ps2_scancode = get_scan_code ();
                     if (0 == ps2_scancode)
                     {
-                        tail -= 2;
+                        kb_tail -= 2;
                         resp = 0x00300600;
-                        goto send_response;
+                        goto send_kb_response;
                     }
 
                     if ((mod = get_modifier (ps2_scancode, 1)))
                     {
                         modifiers &= ~mod;
                         resp = 0x00300600;
-                        goto send_response;
+                        goto send_kb_response;
                     }
                     resp = 0x00280500;
                 }
@@ -445,7 +673,7 @@ int main (void)
                     {
                         modifiers |= mod;
                         resp = 0x00300600;
-                        goto send_response;
+                        goto send_kb_response;
                     }
                     resp = 0x00280400;
                 }
@@ -457,16 +685,16 @@ int main (void)
 
                 if (0 == ps2_scancode)
                 {
-                    tail--; /* put the code back */
+                    kb_tail--; /* put the code back */
                     resp = 0x00300600;
-                    goto send_response;
+                    goto send_kb_response;
                 }
 
                 if ((mod = get_modifier (ps2_scancode, 0)))
                 {
                     modifiers &= ~mod;
                     resp = 0x00300600;
-                    goto send_response;
+                    goto send_kb_response;
                 }
 
                 resp = 0x00280500;
@@ -479,16 +707,72 @@ int main (void)
                 LED_ON;
             }
 
-send_response:
+send_kb_response:
             _delay_us (TIMING*4.5);
             send_response (resp & 0x000000FF, (resp & 0x0000FF00)>>8, (resp & 0x00FF0000)>>16);
         }
         else if (val == 0x00000022)
         {
-            /* TODO: if PS2 mouse available send data */
             uint32_t resp = 0x00300600;
+#if USE_MOUSE
+            uint8_t  ps2_mouse_code[3];
+
+            ps2_mouse_code[0] = get_mouse_code (); /* status */
+            if (0 == ps2_mouse_code[0])
+                goto send_ms_response;
+
+            ps2_mouse_code[1] = get_mouse_code (); /* x */
+            if (0 == ps2_mouse_code[1])
+            {
+                ms_tail--; /* put status byte back */
+                goto send_ms_response;
+            }
+            ps2_mouse_code[2] = get_mouse_code (); /* y */
+            if (0 == ps2_mouse_code[2])
+            {
+                ms_tail -= 2; /* put status and x byte back */
+                goto send_ms_response;
+            }
+
+            resp = 0x00200400;
+
+            /* right button */
+            if (ps2_mouse_code[0] & 0x01)
+                resp |= 0x00001000;
+            else
+                resp &= ~0x00001000;
+
+            /* left button */
+            if (ps2_mouse_code[0] & 0x02)
+                resp |= 0x00000002;
+            else
+                resp &= ~0x00000002;
+
+            /* x movement */
+            if (ps2_mouse_code[1])
+                resp |= 0x04;
+
+            /* +/- */
+            if (ps2_mouse_code[0] & 0x10)
+                resp |= 0x1f8;
+
+            /* y movement */
+            if (ps2_mouse_code[2])
+                resp |= 0x2000;
+
+            if (ps2_mouse_code[0] & 0x20)
+                resp |= 0xFC000;
+
+            /* PS2 mouse cmd is ready */
+
+send_ms_response:
+#endif
             _delay_us (TIMING*4.5);
             send_response (resp & 0x000000FF, (resp & 0x0000FF00)>>8, (resp & 0x00FF0000)>>16);
+        }
+        else if (val == 0x00000FDE)
+        {
+            /* KB reset */
         }
         _delay_us (TIMING);
     }
